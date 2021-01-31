f1050af1201112c5c304f66bac458b65123e6d89
woofbike <jazunka+wfbk@gmail.com>
2021-01-31 13:49:46 +0100

    minus 2 hours

---
+++ Assets/Scripts/Particles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+
+    Transform player;
+
+    ParticleSystem ps;
+
+    void Start() {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update() {
+        
+    }
+}
